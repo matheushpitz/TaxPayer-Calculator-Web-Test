9a4b2e8acf603825448481f46c4c1b07626bce85
Matheus Pitz <matheushenriquepitz@gmail.com>
2020-02-21 15:29:43 -0300

    Changed to await/async

--- src/service/payer.service.js
+++ src/service/payer.service.js
@@ -1,11 +1,19 @@
 export class PayerService {
 
-    static add(data) {
-        return fetch(process.env.REACT_APP_API_HOST+'taxpayer/add', {method: 'POST', body: JSON.stringify(data), headers: {'Content-Type': 'application/json'}}).then((response) => response.json());
+    static async add(data) {
+        try {
+            return (await fetch(process.env.REACT_APP_API_HOST+'taxpayer/add', {method: 'POST', body: JSON.stringify(data), headers: {'Content-Type': 'application/json'}})).json();            
+        } catch(e) {
+            throw e;
+        }        
     }
 
-    static has(cpf) {
-        return fetch(process.env.REACT_APP_API_HOST+'taxpayer/has?cpf=' + cpf).then((response) => response.json());
+    static async has(cpf) {
+        try {
+            return (await fetch(process.env.REACT_APP_API_HOST+'taxpayer/has?cpf=' + cpf)).json();            
+        } catch(e) {
+            throw e;
+        }        
     }
 
 }
